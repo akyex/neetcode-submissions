7f39d8b69090d2750f095e8095569d2e09c5096e
Kye <ruka.akx@gmail.com>
2026-09-14 17:17:31 +1000

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/baseball-game/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+        for element in operations:
+            if element == 'C':
+                record.pop()
+            elif element == 'D':
+                num = record[-1]
+                record.append(num*2)
+            elif element == '+':
+                addednum = record[-2] + record[-1]
+                record.append(addednum)
+            else: 
+                record.append(int(element))
+        
+        total = 0
+        for value in record:
+            total += value
+        
+        return total
