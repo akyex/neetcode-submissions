7dca4155cd29227d9ebb93f6de52a53c94048164
Kye <ruka.akx@gmail.com>
2026-09-15 15:05:51 +1000

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minstack:
+            curr_min = self.minstack[-1]
+        else:
+            curr_min = val
+        num_check = min(val, curr_min)
+        self.minstack.append(num_check)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minstack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
